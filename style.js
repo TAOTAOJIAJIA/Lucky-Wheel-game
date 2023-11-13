be63315d4c7f83c3a4752f99f693ce9240101ea3
qjy <201426350@qq.com>
2023-11-13 20:42:02 +0800

    123

--- style.js
+++ style.js
@@ -6,6 +6,11 @@ let pauseRunBtn = document.getElementById("pause-run");
 let speedUpBtn = document.getElementById("speed-up");
 let reverseRunBtn = document.getElementById("reverse-run");
 let circleBtn = document.getElementById("circle");
+let speedMultiplier = 0.01; // 初始速度乘数
+let user = document.getElementById("userInput");
+let basic = 1800;
+let text = "";
+var randomIndex = 0;
 
 // 这里是奖项的名字
 let prize = ["1号大奖","2号大奖","3号大奖","4号大奖","5号大奖","6号大奖","7号大奖","未中奖"];
@@ -25,29 +30,74 @@ for(let i = 0 ; i < textAll.length ; i++){
 var isFlag = false;
 var pauseRunBtnIsOk = false;
 let timer = null;
-function run(angle){
-    let begin = 0; 
-    
-    let basic = 1800;
-    pauseRunBtnIsOk = true;
-    timer = setInterval(function(){
-        if(begin >= (basic+angle)){
-            isFlag=true;
-            pauseRunBtnIsOk = false;
-            clearInterval(timer);
-        }
-        wapper.style.transform="rotate("+(begin)+"deg)";
-        // 这是一个算法 可以出现转盘又很快到慢慢变慢的效果
-        begin+=Math.ceil(basic+angle-begin)*0.1;
+var reverseRunBtnIsOk = false;
+var speedUpBtnIsOk = false;
 
-    },70);
+//开始的模态框
+function showModal() {
+    document.getElementById('customModal').style.display = 'block';
+}
+
+function hideModal() {
+    if(user.value == "")
+        alert("请输入您的姓名");
+    else
+        document.getElementById('customModal').style.display = 'none';
+}
+
+function handleInput() {
+    if(user.value == "")
+        alert("请输入您的姓名");
+    else
+        {
+        console.log('User entered:', user);
+        hideModal();
+        }
+    
 }
 
 
+//转动函数
+function run(angle){
+    let begin = 0; 
+    
+    pauseRunBtnIsOk = true;
+    speedUpBtnIsOk = false;
+    timer = setInterval(function(){
+        if(begin >= (basic+angle)){
+            if(randomIndex == 7){
+                alert("很遗憾，您没有中奖");
+            }
+            else
+                alert("恭喜您抽中"+text);
+            speedMultiplier = 0.01;
+            basic = 1800;
+            isFlag=true;
+            pauseRunBtnIsOk = false;
+            speedUpBtnIsOk = false;
+            reverseRunBtnIsOk = false;
+            clearInterval(timer);
+        }
+        // console.log(begin);
+        // console.log(-begin);
+        if(!reverseRunBtnIsOk)
+            wapper.style.transform="rotate("+(begin)+"deg)";
+        else
+            wapper.style.transform="rotate("+(-begin)+"deg)";
+        // 这是一个算法 可以出现转盘由很快到慢慢变慢的效果
+        begin+=Math.ceil((basic+angle-begin)*speedMultiplier);
 
+    },70);
+
+    
+
+}
+
+//选中一个幸运值  并启动   转动函数 
 function getLucky(){
 
     let weightRandom = parseInt(Math.random()*30);
+
     // 合并
     let concatAfterArr = prizeWeight.concat(weightRandom);
     
@@ -55,11 +105,11 @@ function getLucky(){
     let  sortedWeightArr  = concatAfterArr.sort(function(a,b){ return a-b });
 
     // randomIndex是奖项的索引 结果是【1,7】
-    var randomIndex = sortedWeightArr.indexOf(weightRandom); 
+    randomIndex = sortedWeightArr.indexOf(weightRandom); 
     randomIndex = Math.min(randomIndex, prize.length -1); 
 
     // 获奖的内容
-    let text = prize[randomIndex];
+    text = prize[randomIndex];
 
     switch(randomIndex){
         case 0:
@@ -90,12 +140,14 @@ function getLucky(){
         
 }
 
+//抽奖按钮
 circleBtn.onclick=function(){
     if(isFlag){
         isFlag=false;
         getLucky();
     }
 };
+
 // 开始按钮
 startRunBtn.onclick=function(){
     if(isFlag){
@@ -103,21 +155,29 @@ startRunBtn.onclick=function(){
         getLucky();
     }
 };
+
+//开始游戏
 var isStart = true;
 startGameBtn.onclick=function(){
     if(isStart){
         isStart=false;
         isFlag=true;
-        let name = prompt("请输入您的名字：");
-        while(!name){
-            name = prompt("请重新输入您的名字：");
-        }
+        document.getElementById('customModal').style.display = 'block';
+       
+        
+        //
+        // let name = prompt("请输入您的名字：");
+        // while(!name){
+        //     name = prompt("请重新输入您的名字：");
+        // }
         startGameBtn.style.backgroundColor = "dimgray";
         startRunBtn.style.backgroundColor = "#f2552e";
         pauseRunBtn.style.backgroundColor = "#f2552e";
         speedUpBtn.style.backgroundColor = "#f2552e";
         reverseRunBtn.style.backgroundColor = "#f2552e";
+        //将“开始游戏”按钮的鼠标光标设置为“默认”,箭头形状,不可以点击
         startGameBtn.style.cursor = "default";
+        //将“开始旋转 等”按钮的鼠标光标设置为“指向”,手形，表示可以点击
         startRunBtn.style.cursor = "pointer";
         pauseRunBtn.style.cursor = "pointer";
         speedUpBtn.style.cursor = "pointer";
@@ -127,6 +187,8 @@ startGameBtn.onclick=function(){
         
 };
 
+
+//暂停按钮
 pauseRunBtn.onclick=function(){
     if(pauseRunBtnIsOk){
         isFlag=true;
@@ -136,7 +198,33 @@ pauseRunBtn.onclick=function(){
     else{
         alert("当前状态下无法使用该功能！");
     }
-
-
 }
+
+//加速按钮
+speedUpBtn.onclick = function(){
+    speedUpBtnIsOk = true;
+    if(isFlag && speedUpBtnIsOk){
+        console.log("前"+speedMultiplier);
+        speedMultiplier = 0.2;
+        console.log("后"+speedMultiplier);
+        isFlag=false;
+        basic = basic*10;
+        getLucky();
+        console.log("后后"+speedMultiplier);
+        }
+    else
+        alert("当前状态下无法使用该功能！");
+}
+
+//反向按钮
+reverseRunBtn.onclick = function(){
+    reverseRunBtnIsOk = true;
+    if(isFlag){
+        isFlag=false;
+        getLucky();
+    }
+}
+
+    
+
 
